44824c4c0a7937b3151b6ffe6600a9f78354b892
刘广海 <1253490794@qq.com>
2020-10-12 20:46:03 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Tabs, TabPane } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/global.css'
+
+Vue.use(Tabs)
+Vue.use(TabPane)
 
 Vue.config.productionTip = false
 
